ed1e4d2c4863334aa3b93e2a270c9fbf5abda5f4
iaincrawf <iain.crawford@linn.co.uk>
2021-12-02 18:10:11 +0000

    fix merge mistake

--- src/IoC/PersistenceExtensions.cs
+++ src/IoC/PersistenceExtensions.cs
@@ -20,7 +20,7 @@
                 .AddTransient<ITransactionManager, TransactionManager>()
                 .AddTransient<IRepository<Thing, int>, ThingRepository>()
                 .AddTransient<IRepository<SigningLimit, int>, SigningLimitRepository>()
-                .AddTransient<IRepository<PartSupplier, PartSupplierKey>, EntityFrameworkRepository<PartSupplier, PartSupplierKey>>(r => new EntityFrameworkRepository<PartSupplier, PartSupplierKey>(r.GetService<ServiceDbContext>()?.PartSuppliers));
+                .AddTransient<IRepository<PartSupplier, PartSupplierKey>, EntityFrameworkRepository<PartSupplier, PartSupplierKey>>(r => new EntityFrameworkRepository<PartSupplier, PartSupplierKey>(r.GetService<ServiceDbContext>()?.PartSuppliers))
                 .AddTransient<IRepository<PurchaseOrder, int>, PurchaseOrderRepository>();
 
             // Could also be
