c20eddbe78bea48ac46675a63dd581da9af8deee
lewisre <lewisre@PC1152>
2021-12-03 15:46:19 +0000

    get something rendering

--- src/Service.Host/client/src/components/Root.js
+++ src/Service.Host/client/src/components/Root.js
@@ -4,8 +4,8 @@ import { Route, Redirect, Switch } from 'react-router';
 import { OidcProvider } from 'redux-oidc';
 import { ConnectedRouter } from 'connected-react-router';
 import { Navigation } from '@linn-it/linn-form-components-library';
-import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import MomentUtils from '@date-io/moment';
+// import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+// import MomentUtils from '@date-io/moment';
 import PropTypes from 'prop-types';
 import history from '../history';
 import App from './App';
@@ -13,14 +13,14 @@ import Callback from './Callback';
 import userManager from '../helpers/userManager';
 import 'typeface-roboto';
 import NotFoundPage from './NotFoundPage';
-import SigningLimits from './SigningLimits';
+// import SigningLimits from './SigningLimits';
 
 const Root = ({ store }) => (
     <div>
         <div className="padding-top-when-not-printing">
             <Provider store={store}>
                 <OidcProvider store={store} userManager={userManager}>
-                    <MuiPickersUtilsProvider utils={MomentUtils}>
+                    {/* <MuiPickersUtilsProvider utils={MomentUtils}> */}
                         <ConnectedRouter history={history}>
                             <div>
                                 <Navigation />
@@ -39,11 +39,11 @@ const Root = ({ store }) => (
                                 />
                                 <Switch>
                                     <Route exact path="/purchasing" component={App} />
-                                    <Route
+                                    {/* <Route
                                         exact
                                         path="/purchasing/signing-limits"
                                         component={SigningLimits}
-                                    />
+                                    /> */}
                                     <Route
                                         exact
                                         path="/purchasing/signin-oidc-client"
@@ -53,7 +53,7 @@ const Root = ({ store }) => (
                                 </Switch>
                             </div>
                         </ConnectedRouter>
-                    </MuiPickersUtilsProvider>
+                    {/* </MuiPickersUtilsProvider> */}
                 </OidcProvider>
             </Provider>
         </div>
