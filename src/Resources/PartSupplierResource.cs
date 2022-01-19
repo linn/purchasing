a0c05691d15e5bcd445871c737d254d6d381be53
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2022-01-19 12:28:00 +0000

    remove unused depds

--- src/Resources/PartSupplierResource.cs
+++ src/Resources/PartSupplierResource.cs
@@ -1,8 +1,5 @@
 ﻿namespace Linn.Purchasing.Resources
 {
-    using System;
-    using System.Collections.Generic;
-
     using Linn.Common.Resources;
 
     public class PartSupplierResource : HypermediaResource
