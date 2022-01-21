ce53e15189b5a0c49e0cec19a1dc2a9e17597210
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2022-01-21 15:05:27 +0000

    fix typo

--- src/Service.Host/client/src/middleware/itemCreated.js
+++ src/Service.Host/client/src/middleware/itemCreated.js
@@ -7,8 +7,7 @@ export default () => next => action => {
 
     if (action.type.startsWith('RECEIVE_NEW_')) {
         if (
-            action.type !==
-                actionTypes.RECEIVE_NEW_PREFERRED_SUPPLIER_CHANGERECEIVE_NEW_SIGNING_LIMIT ||
+            action.type !== actionTypes.RECEIVE_NEW_SIGNING_LIMIT ||
             action.type !== actionTypes.RECEIVE_NEW_PREFERRED_SUPPLIER_CHANGE
         ) {
             history.push(utilities.getSelfHref(action.payload.data));
