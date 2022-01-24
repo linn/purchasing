10da2ca1bf6ae6d3fa5930cfde0ab21e93f58387
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2022-01-24 16:39:41 +0000

    remove using that sneaked in

--- src/Domain.LinnApps/PartSuppliers/PartSupplierService.cs
+++ src/Domain.LinnApps/PartSuppliers/PartSupplierService.cs
@@ -3,7 +3,6 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Xml.Xsl;
 
     using Linn.Common.Authorisation;
     using Linn.Common.Persistence;
