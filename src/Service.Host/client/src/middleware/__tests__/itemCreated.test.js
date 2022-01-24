7f1dad6da551661cd0f3b2de4b3196a9d867fe35
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2022-01-24 17:02:31 +0000

    tests to catch the middleware bug I introduced earlier

---
+++ src/Service.Host/client/src/middleware/__tests__/itemCreated.test.js
@@ -0,0 +1,50 @@
+/**
+ * @jest-environment jsdom
+ */
+import itemCreated from '../itemCreated';
+import history from '../../history';
+
+jest.mock('../../history', () => ({
+    push: jest.fn()
+}));
+
+const historyPushSpy = jest.spyOn(history, 'push');
+const fakeStore = { dispatch: jest.fn() };
+const fakeNext = jest.fn();
+
+beforeEach(() => {
+    jest.clearAllMocks();
+});
+
+describe('When RECEIVE_NEW_SIGNING_LIMIT...', () => {
+    test('should not redirect', () => {
+        expect(historyPushSpy).not.toBeCalled();
+    });
+
+    itemCreated(fakeStore)(fakeNext)({ type: 'RECEIVE_NEW_SIGNING_LIMIT', payload: {} });
+    expect(historyPushSpy).not.toBeCalled();
+});
+
+describe('When RECEIVE_NEW_SIGNING_LIMIT...', () => {
+    test('should not redirect', () => {
+        expect(historyPushSpy).not.toBeCalled();
+    });
+
+    itemCreated(fakeStore)(fakeNext)({
+        type: 'RECEIVE_NEW_PREFERRED_SUPPLIER_CHANGE',
+        payload: {}
+    });
+    expect(historyPushSpy).not.toBeCalled();
+});
+
+describe('When any other RECEIVE_NEW_ action', () => {
+    test('should redirect', () => {
+        expect(historyPushSpy).not.toBeCalled();
+    });
+
+    itemCreated(fakeStore)(fakeNext)({
+        type: 'RECEIVE_NEW_TYPE',
+        payload: { data: { links: [{ rel: 'self', href: '/location' }] } }
+    });
+    expect(historyPushSpy).toBeCalledWith('/location');
+});
