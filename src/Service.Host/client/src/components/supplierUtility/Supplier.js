715128b78a1464a064bfca384c85ad25c24d8f0e
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2022-02-02 15:07:35 +0000

    placeholder view page

--- src/Service.Host/client/src/components/supplierUtility/Supplier.js
+++ src/Service.Host/client/src/components/supplierUtility/Supplier.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { collectionSelectorHelpers, itemSelectorHelpers, Page, Typeahead } from '@linn-it/linn-form-components-library';
+import { itemSelectorHelpers, Page, Loading } from '@linn-it/linn-form-components-library';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+import { useParams } from 'react-router-dom';
 
 import supplierActions from '../../actions/supplierActions';
 import history from '../../history';
@@ -11,12 +12,36 @@ import config from '../../config';
 function Supplier() {
     const dispatch = useDispatch();
 
+    const { id } = useParams();
+    const supplier = useSelector(state => itemSelectorHelpers.getItem(state.supplier));
+    const supplierLoading = useSelector(state =>
+        itemSelectorHelpers.getItemLoading(state.supplier)
+    );
+
+    useEffect(() => {
+        if (id) {
+            dispatch(supplierActions.fetch(id));
+        }
+    }, [id, dispatch]);
     return (
         <Page history={history} homeUrl={config.appRoot}>
             <Grid container spacing={3}>
-                <Grid item xs={12}>
-                    <Typography variant="h3">Supplier</Typography>
-                </Grid>
+                {supplierLoading ? (
+                    <>
+                        <Grid item xs={12}>
+                            <Loading />
+                        </Grid>
+                    </>
+                ) : (
+                    supplier && (
+                        <>
+                            <Grid item xs={12}>
+                                <Typography variant="h3">Supplier</Typography>
+                                <Typography variant="h3">{supplier.id}</Typography>
+                            </Grid>
+                        </>
+                    )
+                )}
             </Grid>
         </Page>
     );
