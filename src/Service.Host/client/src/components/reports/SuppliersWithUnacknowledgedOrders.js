38587901e90d0e51ccc7a67a20900250e76539b8
Richard Phillips <richard.phillips@linn.co.uk>
2022-02-21 09:42:00 +0000

    external links experiment

--- src/Service.Host/client/src/components/reports/SuppliersWithUnacknowledgedOrders.js
+++ src/Service.Host/client/src/components/reports/SuppliersWithUnacknowledgedOrders.js
@@ -127,6 +127,7 @@ function SuppliersWithUnacknowledgedOrders() {
                                     showTitle={false}
                                     showTotals={false}
                                     placeholderRows={0}
+                                    hasExternalLinks
                                     placeholderColumns={0}
                                 />
                             )}
