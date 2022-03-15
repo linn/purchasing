4ea8060e6b3680894ade9a97f44e93ce5431fcdf
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2022-03-15 09:44:48 +0000

    unit tests

---
+++ tests/Unit/Domain.LinnApps.Tests/PartSupplierServiceTests/WhenBulkUpdatingLeadTimesAndNotAuthorised.cs
@@ -0,0 +1,39 @@
+﻿namespace Linn.Purchasing.Domain.LinnApps.Tests.PartSupplierServiceTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+
+    using FluentAssertions;
+
+    using Linn.Purchasing.Domain.LinnApps.Exceptions;
+    using Linn.Purchasing.Domain.LinnApps.PartSuppliers;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public class WhenBulkUpdatingLeadTimesAndNotAuthorised : ContextBase
+    {
+        private Action action;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.action = () => this.Sut.BulkUpdateLeadTimes(new List<LeadTimeUpdateModel>(), new List<string>());
+        }
+
+        [Test]
+        public void ShouldThrowUnauthorisedActionException()
+        {
+            this.action.Should().Throw<UnauthorisedActionException>();
+        }
+
+        [Test]
+        public void ShouldNotUpdate()
+        {
+            this.PartSupplierRepository.DidNotReceive()
+                .FindBy(Arg.Any<Expression<Func<PartSupplier, bool>>>());
+        }
+    }
+}
