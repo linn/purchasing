0b3ae10969a96321156b5dbe5878543ad2a668c0
Lewis Renfrew <LewisRe@linn.co.uk>
2022-03-30 14:15:07 +0100

    serve up app at /print endpoint

--- src/Service/Modules/PurchaseOrderModule.cs
+++ src/Service/Modules/PurchaseOrderModule.cs
@@ -76,6 +76,7 @@
 
             this.Get("/purchasing/purchase-orders/reqs", this.SearchReqs);
             this.Get("/purchasing/purchase-orders/reqs/states", this.GetReqStates);
+            this.Get("/purchasing/purchase-orders/reqs/print", this.GetApp);
 
             this.Get("/purchasing/purchase-orders/reqs/application-state", this.GetReqApplicationState);
             this.Get("/purchasing/purchase-orders/reqs/{id:int}", this.GetReq);
