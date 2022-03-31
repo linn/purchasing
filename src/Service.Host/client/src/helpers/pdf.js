061ee8e15255d5160c27481297b8976b063ba2d4
Lewis Renfrew <LewisRe@linn.co.uk>
2022-03-31 09:34:55 +0100

    use better balance between quality and speed to avoid long waits

--- src/Service.Host/client/src/helpers/pdf.js
+++ src/Service.Host/client/src/helpers/pdf.js
@@ -4,25 +4,24 @@ import { jsPDF as JsPDF } from 'jspdf';
 export const savePdf = async ref => {
     const element = ref.current;
     const canvas = await html2canvas(element, {
-        quality: 4,
-        scale: 5
+        quality: 5,
+        scale: 2
     });
-    const data = canvas.toDataURL('image/png');
-
+    const data = canvas.toDataURL('image/jpeg');
     const pdf = new JsPDF('p', 'pt', 'a4', true);
     const imgProperties = pdf.getImageProperties(data);
     const pdfWidth = pdf.internal.pageSize.getWidth();
     const pdfHeight = (imgProperties.height * pdfWidth) / imgProperties.width;
 
-    pdf.addImage(data, 'PNG', 0, 0, pdfWidth, pdfHeight, '', 'FAST');
+    pdf.addImage(data, 'jpeg', 0, 0, pdfWidth, pdfHeight, '', 'FAST');
     pdf.save();
 };
 
 export const emailPdf = async (ref, sendEmailCallback) => {
     const element = ref.current;
     const canvas = await html2canvas(element, {
-        quality: 4,
-        scale: 5
+        quality: 5,
+        scale: 2
     });
     const data = canvas.toDataURL('image/png');
 
