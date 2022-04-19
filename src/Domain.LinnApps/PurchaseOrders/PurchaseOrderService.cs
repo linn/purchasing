5b1319ee1aa9c3250f45d95c6cdd41391c5872a2
iaincrawf <iain.crawford@linn.co.uk>
2022-04-19 17:58:47 +0100

    cancelled fleshed out in domain

--- src/Domain.LinnApps/PurchaseOrders/PurchaseOrderService.cs
+++ src/Domain.LinnApps/PurchaseOrders/PurchaseOrderService.cs
@@ -1,28 +1,39 @@
 ﻿namespace Linn.Purchasing.Domain.LinnApps.PurchaseOrders
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
 
     using Linn.Common.Authorisation;
     using Linn.Common.Persistence;
+    using Linn.Common.Proxy.LinnApps;
     using Linn.Purchasing.Domain.LinnApps.Exceptions;
+    using Linn.Purchasing.Domain.LinnApps.ExternalServices;
 
     public class PurchaseOrderService : IPurchaseOrderService
     {
         private readonly IAuthorisationService authService;
+
         private readonly IRepository<CancelledOrderDetail, int> cancelledDetailRepository;
+
+        private readonly IDatabaseService databaseService;
+
         private readonly IRepository<PurchaseOrder, int> orderRepository;
 
-
+        private readonly IPurchaseLedgerPack purchaseLedgerPack;
 
         public PurchaseOrderService(
             IAuthorisationService authService,
             IRepository<CancelledOrderDetail, int> cancelledDetailRepository,
-            IRepository<PurchaseOrder, int> orderRepository)
+            IRepository<PurchaseOrder, int> orderRepository,
+            IPurchaseLedgerPack purchaseLedgerPack,
+            IDatabaseService databaseService)
         {
             this.authService = authService;
             this.cancelledDetailRepository = cancelledDetailRepository;
             this.orderRepository = orderRepository;
+            this.purchaseLedgerPack = purchaseLedgerPack;
+            this.databaseService = databaseService;
         }
 
         public void AllowOverbook(PurchaseOrder current, PurchaseOrder updated, IEnumerable<string> privileges)
@@ -36,44 +47,6 @@
             current.OverbookQty = updated.OverbookQty;
         }
 
-        public PurchaseOrder Update(PurchaseOrder current, PurchaseOrder updated, IEnumerable<string> privileges)
-        {
-            if (!this.authService.HasPermissionFor(AuthorisedAction.PurchaseOrderUpdate, privileges))
-            {
-                throw new UnauthorisedActionException("You are not authorised to update purchase orders");
-            }
-
-            this.UpdateTopLevelProperties(current, updated);
-            //update details etc
-
-            return current;
-        }
-
-        public PurchaseOrder Create(PurchaseOrder newOrder, IEnumerable<string> privileges)
-        {
-            if (!this.authService.HasPermissionFor(AuthorisedAction.PurchaseOrderCreate, privileges))
-            {
-                throw new UnauthorisedActionException("You are not authorised to create purchase orders");
-            }
-
-            //create order
-
-            this.orderRepository.Add(newOrder);
-            //will this add the details etc?
-
-            //create detail(s)
-
-
-            //create deliveries maybe only if split deliveries is 0? Are split delivieres pl deliveries or something else?
-
-            //create pl_order_postings
-            //select plorp_seq.nextval into v_plorp from dual;
-
-
-
-            return newOrder;
-        }
-
         public PurchaseOrder CancelOrder(PurchaseOrder order, int currentUserId, IEnumerable<string> privileges)
         {
             if (!this.authService.HasPermissionFor(AuthorisedAction.PurchaseOrderUpdate, privileges))
@@ -83,16 +56,65 @@
 
             order.Cancelled = "Y";
 
-            //for each line on order
-            //create cancelled detail 
+            // for each line on order
+            var currentLedgerPeriod = this.purchaseLedgerPack.GetLedgerPeriod();
+            foreach (var detail in order.Details)
+            {
+                var cancelledDetail = new CancelledOrderDetail
+                                          {
+                                              Id = this.databaseService.GetNextVal("PLOC_SEQ"),
+                                              OrderNumber = detail.OrderNumber,
+                                              LineNumber = detail.Line,
+                                              DateCancelled = DateTime.Today,
+                                              PeriodCancelled = currentLedgerPeriod,
+                                              CancelledById = currentUserId,
+                                              ReasonCancelled = "todo", // detail.ReasonCancelled,
+                                              ValueCancelled = detail.BaseDetailTotal
+                                          };
+
+                // todo check that baseDetailTotal is   round(nvl(v_qty_outstanding, 0) * :new.base_our_price, 2)
+                detail.CancelledDetails.Add(cancelledDetail);
+
+                // check above works and this can then go:
+                // this.cancelledDetailRepository.Add(cancelledDetail);
+            }
 
             return order;
         }
 
-        private void UpdateTopLevelProperties(PurchaseOrder current, PurchaseOrder updated)
+        public PurchaseOrder Create(PurchaseOrder newOrder, IEnumerable<string> privileges)
         {
-            current.AuthorisedById = updated.AuthorisedById;
-            //current.
+            //todo move to facade
+            if (!this.authService.HasPermissionFor(AuthorisedAction.PurchaseOrderCreate, privileges))
+            {
+                throw new UnauthorisedActionException("You are not authorised to create purchase orders");
+            }
+
+            // create order
+            this.orderRepository.Add(newOrder);
+
+            // will this add the details etc?
+
+            // create detail(s)
+
+            // create deliveries maybe only if split deliveries is 0? Are split delivieres pl deliveries or something else?
+
+            // create pl_order_postings
+            // select plorp_seq.nextval into v_plorp from dual;
+            return newOrder;
+        }
+
+        public PurchaseOrder Update(PurchaseOrder current, PurchaseOrder updated, IEnumerable<string> privileges)
+        {
+            if (!this.authService.HasPermissionFor(AuthorisedAction.PurchaseOrderUpdate, privileges))
+            {
+                throw new UnauthorisedActionException("You are not authorised to update purchase orders");
+            }
+
+            this.UpdateTopLevelProperties(current, updated);
+            this.UpdateDetails(current.Details, updated.Details);
+
+            return current;
         }
 
         private void UpdateDetails(IList<PurchaseOrderDetail> currentDetails, IList<PurchaseOrderDetail> updatedDetails)
@@ -103,13 +125,22 @@
                 {
                     var current = currentDetails.First(x => x.Line == detail.Line);
                     current.PartNumber = detail.PartNumber;
-                    //update rest
+
+                    // update rest
+                    // + update deliveries etc
                 }
                 else
                 {
                     currentDetails.Add(detail);
                 }
             }
+        }
+
+        private void UpdateTopLevelProperties(PurchaseOrder current, PurchaseOrder updated)
+        {
+            current.AuthorisedById = updated.AuthorisedById;
+
+            // current.
         }
     }
 }
--- src/Facade/Services/PurchaseOrderFacadeService.cs
+++ src/Facade/Services/PurchaseOrderFacadeService.cs
@@ -9,10 +9,12 @@
     using Linn.Purchasing.Domain.LinnApps.PurchaseOrders;
     using Linn.Purchasing.Resources;
 
-    public class PurchaseOrderFacadeService
-        : FacadeResourceService<PurchaseOrder, int, PurchaseOrderResource, PurchaseOrderResource>
+    public class
+        PurchaseOrderFacadeService : FacadeResourceService<PurchaseOrder, int, PurchaseOrderResource,
+            PurchaseOrderResource>
     {
         private readonly IPurchaseOrderService domainService;
+
         private readonly IRepository<OverbookAllowedByLog, int> overbookAllowedByLogRepository;
 
         public PurchaseOrderFacadeService(
@@ -28,43 +30,33 @@
         }
 
         protected override PurchaseOrder CreateFromResource(
-            PurchaseOrderResource resource, 
+            PurchaseOrderResource resource,
             IEnumerable<string> privileges = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void DeleteOrObsoleteResource(PurchaseOrder entity, IEnumerable<string> privileges = null)
         {
             throw new NotImplementedException();
         }
 
         protected override void SaveToLogTable(
-            string actionType, 
+            string actionType,
             int userNumber,
-            PurchaseOrder entity, 
-            PurchaseOrderResource resource, 
+            PurchaseOrder entity,
+            PurchaseOrderResource resource,
             PurchaseOrderResource updateResource)
         {
+            // if is overbook form
             var log = new OverbookAllowedByLog
-            {
-                OrderNumber = entity.OrderNumber,
-                OverbookQty = entity.OverbookQty,
-                OverbookDate = DateTime.Now,
-                OverbookGrantedBy = userNumber
-            };
+                          {
+                              OrderNumber = entity.OrderNumber,
+                              OverbookQty = entity.OverbookQty,
+                              OverbookDate = DateTime.Now,
+                              OverbookGrantedBy = userNumber
+                          };
             this.overbookAllowedByLogRepository.Add(log);
-        }
-
-        protected override void DeleteOrObsoleteResource(
-            PurchaseOrder entity, 
-            IEnumerable<string> privileges = null)
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override void UpdateFromResource(
-            PurchaseOrder entity, 
-            PurchaseOrderResource updateResource, 
-            IEnumerable<string> privileges = null)
-        {
-            var updated = this.BuildEntityFromResourceHelper(updateResource);
-            this.domainService.AllowOverbook(entity, updated, privileges);
         }
 
         protected override Expression<Func<PurchaseOrder, bool>> SearchExpression(string searchTerm)
@@ -72,18 +64,29 @@
             return x => x.OrderNumber.ToString().Contains(searchTerm);
         }
 
+        protected override void UpdateFromResource(
+            PurchaseOrder entity,
+            PurchaseOrderResource updateResource,
+            IEnumerable<string> privileges = null)
+        {
+            var updated = this.BuildEntityFromResourceHelper(updateResource);
+
+            //check if overbook form or not, direct to different domain methods
+            this.domainService.AllowOverbook(entity, updated, privileges);
+        }
+
         private PurchaseOrder BuildEntityFromResourceHelper(PurchaseOrderResource resource)
         {
             return new PurchaseOrder
-            {
-                OrderNumber = resource.OrderNumber,
-                Cancelled = resource.Cancelled,
-                DocumentTypeName = resource.DocumentType,
-                OrderDate = resource.DateOfOrder,
-                Overbook = resource.Overbook,
-                OverbookQty = resource.OverbookQty,
-                SupplierId = resource.SupplierId,
-            };
+                       {
+                           OrderNumber = resource.OrderNumber,
+                           Cancelled = resource.Cancelled,
+                           DocumentTypeName = resource.DocumentType,
+                           OrderDate = resource.DateOfOrder,
+                           Overbook = resource.Overbook,
+                           OverbookQty = resource.OverbookQty,
+                           SupplierId = resource.SupplierId
+                       };
         }
     }
 }
