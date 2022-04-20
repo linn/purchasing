bbef137ae41424c1bfd7d9e5257228402a7c9a35
Richard Phillips <richardiphillips@users.noreply.github.com>
2022-04-20 16:44:30 +0100

    add missing tests (#167)
    upgrade mimekit.
    upgrade identity server within major version

---
+++ tests/Integration/Integration.Tests/PrefSupReceiptsReportModuleTests/ContextBase.cs
@@ -0,0 +1,37 @@
+﻿namespace Linn.Purchasing.Integration.Tests.PrefSupReceiptsReportModuleTests
+{
+    using System.Net.Http;
+
+    using Linn.Purchasing.Facade.Services;
+    using Linn.Purchasing.IoC;
+    using Linn.Purchasing.Service.Modules.Reports;
+
+    using Microsoft.Extensions.DependencyInjection;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public abstract class ContextBase
+    {
+        protected HttpClient Client { get; set; }
+
+        protected HttpResponseMessage Response { get; set; }
+
+        protected IPrefSupReceiptsReportFacadeService ReportFacadeService { get; set; }
+
+        [SetUp]
+        public void EstablishContext()
+        {
+            this.ReportFacadeService = Substitute.For<IPrefSupReceiptsReportFacadeService>();
+
+            this.Client = TestClient.With<PrefSupReceiptsReportModule>(
+                services =>
+                    {
+                        services.AddSingleton(this.ReportFacadeService);
+                        services.AddHandlers();
+                    },
+                FakeAuthMiddleware.EmployeeMiddleware);
+        }
+    }
+}
