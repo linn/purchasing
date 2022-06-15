169e8ebe03b6b5ae8a2911e04fe30f8aff29115c
Richard Phillips <richard.phillips@linn.co.uk>
2022-06-15 11:53:23 +0100

    removed obsolete text only resources

--- tests/Unit/Facade.Tests/ResourceBuilderTests/MrReportResourceBuilderTests/WhenBuildingReportResourceForMr.cs
+++ tests/Unit/Facade.Tests/ResourceBuilderTests/MrReportResourceBuilderTests/WhenBuildingReportResourceForMr.cs
@@ -299,28 +299,6 @@
             part1Result.Details.Should().Contain(a => a.Segment == 1);
             var part1Details1Resources = part1Result.Details.Where(a => a.Segment == 0).ToList();
             var originalPart1Details = this.MrHeader1.MrDetails.First(a => a.Segment == 0);
-            part1Details1Resources.First(a => a.Title == "Week").Week0.Should().Be(originalPart1Details.WeekAndYear);
-            part1Details1Resources.First(a => a.Title == "Ending").Week0.Should().Be(originalPart1Details.WeekEnding);
-            part1Details1Resources.First(a => a.Title == "Fixed Build").Week0.Should().Be(originalPart1Details.FixedBuild.ToString());
-            part1Details1Resources.First(a => a.Title == "Assumed Build").Week0.Should().Be(originalPart1Details.AssumedBuild.ToString());
-            part1Details1Resources.First(a => a.Title == "Trigger Build").Week0.Should().Be(originalPart1Details.TriggerBuild.ToString());
-            part1Details1Resources.First(a => a.Title == "Purchases").Week0.Should().Be(originalPart1Details.PurchaseOrders.ToString());
-            part1Details1Resources.First(a => a.Title == "Unauthorised POs").Week0.Should().Be(originalPart1Details.UnauthorisedPurchaseOrders.ToString());
-            part1Details1Resources.First(a => a.Title == "Assumed Purchases").Week0.Should().Be(originalPart1Details.AssumedPurchaseOrders.ToString());
-            part1Details1Resources.First(a => a.Title == "Sales Orders").Week0.Should().Be(originalPart1Details.SalesOrders.ToString());
-            part1Details1Resources.First(a => a.Title == "Sales Forecast").Week0.Should().Be(originalPart1Details.DeliveryForecast.ToString());
-            part1Details1Resources.First(a => a.Title == "Production Reqt").Week0.Should().Be(originalPart1Details.ProductionRequirement.ToString());
-            part1Details1Resources.First(a => a.Title == "Prod For Spares").Week0.Should().Be(originalPart1Details.ProductionRequirementForSpares.ToString());
-            part1Details1Resources.First(a => a.Title == "Prod For NonProd").Week0.Should().Be(originalPart1Details.ProductionRequirementForNonProduction.ToString());
-            part1Details1Resources.First(a => a.Title == "Non Prod Reqt").Week0.Should().Be(originalPart1Details.NonProductionRequirement.ToString());
-            part1Details1Resources.First(a => a.Title == "Spares Reqt").Week0.Should().Be(originalPart1Details.SparesRequirement.ToString());
-            part1Details1Resources.First(a => a.Title == "Status").Week0.Should().Be(originalPart1Details.Status);
-            part1Details1Resources.First(a => a.Title == "Stock").Week0.Should().Be(originalPart1Details.Stock.ToString());
-            part1Details1Resources.First(a => a.Title == "Min Rail").Week0.Should().Be(originalPart1Details.MinRail.ToString());
-            part1Details1Resources.First(a => a.Title == "Max Rail").Week0.Should().Be(originalPart1Details.MaxRail.ToString());
-            part1Details1Resources.Should().NotContain(a => a.Title == "Ideal Stock");
-            part1Details1Resources.First(a => a.Title == "Recom Orders").Week0.Should().Be(originalPart1Details.RecommendedOrders.ToString());
-            part1Details1Resources.Should().NotContain(a => a.Title == "Recom Stock");
 
             part1Details1Resources.First(a => a.Title == "Week").Week0Item.TextValue.Should().Be(originalPart1Details.WeekAndYear);
             part1Details1Resources.First(a => a.Title == "Ending").Week0Item.TextValue.Should().Be(originalPart1Details.WeekEnding);
@@ -342,6 +320,8 @@
             part1Details1Resources.First(a => a.Title == "Min Rail").Week0Item.Value.Should().Be(originalPart1Details.MinRail);
             part1Details1Resources.First(a => a.Title == "Max Rail").Week0Item.Value.Should().Be(originalPart1Details.MaxRail);
             part1Details1Resources.First(a => a.Title == "Recom Orders").Week0Item.Value.Should().Be(originalPart1Details.RecommendedOrders);
+            part1Details1Resources.Should().NotContain(a => a.Title == "Ideal Stock");
+            part1Details1Resources.Should().NotContain(a => a.Title == "Recom Stock");
         }
 
         [Test]
@@ -353,8 +333,8 @@
             part2ResultResource.Details.Should().Contain(a => a.Segment == 1);
             var part2Details1Resources = part2ResultResource.Details.Where(a => a.Segment == 1).ToList();
             var originalPart2Details = this.MrHeader2.MrDetails.First(a => a.Segment == 1);
-            part2Details1Resources.First(a => a.Title == "Week").Week3.Should().Be(originalPart2Details.WeekAndYear);
-            part2Details1Resources.First(a => a.Title == "Ending").Week3.Should().Be(originalPart2Details.WeekEnding);
+            part2Details1Resources.First(a => a.Title == "Week").Week3Item.TextValue.Should().Be(originalPart2Details.WeekAndYear);
+            part2Details1Resources.First(a => a.Title == "Ending").Week3Item.TextValue.Should().Be(originalPart2Details.WeekEnding);
             part2Details1Resources.Should().NotContain(a => a.Title == "Fixed Build");
             part2Details1Resources.Should().NotContain(a => a.Title == "Assumed Build");
             part2Details1Resources.Should().NotContain(a => a.Title == "Trigger Build");
@@ -368,10 +348,10 @@
             part2Details1Resources.Should().NotContain(a => a.Title == "Prod For NonProd");
             part2Details1Resources.Should().NotContain(a => a.Title == "Non Prod Reqt");
             part2Details1Resources.Should().NotContain(a => a.Title == "Spares Reqt");
-            part2Details1Resources.First(a => a.Title == "Status").Week3.Should().Be(originalPart2Details.Status);
-            part2Details1Resources.First(a => a.Title == "Stock").Week3.Should().Be(originalPart2Details.Stock.ToString());
-            part2Details1Resources.First(a => a.Title == "Min Rail").Week3.Should().Be(originalPart2Details.MinRail.ToString());
-            part2Details1Resources.First(a => a.Title == "Max Rail").Week3.Should().Be(originalPart2Details.MaxRail.ToString());
+            part2Details1Resources.First(a => a.Title == "Status").Week3Item.TextValue.Should().Be(originalPart2Details.Status);
+            part2Details1Resources.First(a => a.Title == "Stock").Week3Item.Value.Should().Be(originalPart2Details.Stock);
+            part2Details1Resources.First(a => a.Title == "Min Rail").Week3Item.Value.Should().Be(originalPart2Details.MinRail);
+            part2Details1Resources.First(a => a.Title == "Max Rail").Week3Item.Value.Should().Be(originalPart2Details.MaxRail);
             part2Details1Resources.Should().NotContain(a => a.Title == "Ideal Stock");
             part2Details1Resources.Should().NotContain(a => a.Title == "Recom Orders");
             part2Details1Resources.Should().NotContain(a => a.Title == "Recom Stock");
