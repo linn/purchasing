aef3819e1e4a6f047342c2551696b70a1c432fbc
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2022-06-16 14:37:31 +0100

    wip

--- src/Domain.LinnApps/MaterialRequirements/MrPurchaseOrderDetail.cs
+++ src/Domain.LinnApps/MaterialRequirements/MrPurchaseOrderDetail.cs
@@ -4,7 +4,6 @@
     using System.Collections.Generic;
 
     using Linn.Purchasing.Domain.LinnApps.PartSuppliers;
-    using Linn.Purchasing.Domain.LinnApps.Suppliers;
 
     public class MrPurchaseOrderDetail
     {
---
+++ src/Domain.LinnApps/Reports/MrOrderBookReportService.cs
@@ -0,0 +1,28 @@
+﻿namespace Linn.Purchasing.Domain.LinnApps.Reports
+{
+    using Linn.Common.Persistence;
+    using Linn.Common.Reporting.Models;
+    using Linn.Purchasing.Domain.LinnApps.MaterialRequirements;
+
+    public class MrOrderBookReportService : IMrOrderBookReportService
+    {
+        private readonly IQueryRepository<MrPurchaseOrderDetail> repository;
+
+        private readonly ISingleRecordRepository<MrMaster> mrMaster;
+
+        public MrOrderBookReportService(
+            IQueryRepository<MrPurchaseOrderDetail> repository,
+            ISingleRecordRepository<MrMaster> mrMaster)
+        {
+            this.repository = repository;
+            this.mrMaster = mrMaster;
+        }
+
+        public ResultsModel GetOrderBookReport(int supplierId)
+        {
+            var jobRef = this.mrMaster.GetRecord().JobRef;
+            var data = this.repository.FilterBy(x => x.SupplierId == supplierId && x.JobRef == jobRef);
+            throw new System.NotImplementedException();
+        }
+    }
+}
