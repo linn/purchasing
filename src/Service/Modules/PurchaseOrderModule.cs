6d28be5e88b060d88bf9115af603302c27edf341
iaincrawf <iain.crawford@linn.co.uk>
2022-06-17 13:01:39 +0100

    remove line break

--- src/Service/Modules/PurchaseOrderModule.cs
+++ src/Service/Modules/PurchaseOrderModule.cs
@@ -36,7 +36,6 @@
             app.MapGet("/purchasing/purchase-orders/{orderNumber:int}", this.GetPurchaseOrder);
             app.MapGet("/purchasing/purchase-orders/{orderNumber:int}/html", this.GetPurchaseOrderHtml);
             app.MapPost("/purchasing/purchase-orders/email-pdf", this.EmailOrderPdf);
-
             app.MapPut("/purchasing/purchase-orders/{orderNumber:int}", this.UpdatePurchaseOrder);
         }
 
