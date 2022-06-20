253147c2628043f4e1287103d5c034f88236d53d
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2022-06-20 14:08:51 +0100

    update helpertext

--- src/Service.Host/client/src/components/AcknowledgeOrdersUtility.js
+++ src/Service.Host/client/src/components/AcknowledgeOrdersUtility.js
@@ -463,7 +463,7 @@ function AcknowledgeOrdersUtility() {
                         setSnackbarVisible={setUploadSnackbarVisible}
                         message={uploadMessage}
                         initiallyExpanded={false}
-                        helperText="Upload a csv file with 4 columns, Order Number, Delivery Number, New Advised Date and New Reason. Date must be in a format matching either 31/01/2022 or 31-jan-2022. Advised must be one of the following: ADVISED, AUTO FAIL, AUTO PASS, BROUGHT IN, DECOMMIT, IGNORE, REQUESTED, RESCHEDULE OUT and will default to ADVISED if no value is supplied."
+                        helperText="Upload a csv file with 4 columns, Order Number, Delivery Number, New Advised Date, Qty and New Reason. Date must be in a format matching either 31/01/2022 or 31-jan-2022. Advised must be one of the following: ADVISED, AUTO FAIL, AUTO PASS, BROUGHT IN, DECOMMIT, IGNORE, REQUESTED, RESCHEDULE OUT and will default to ADVISED if no value is supplied."
                     />
                 </Grid>
             </Grid>
