bc1aa4af7566c30340440049f9fe5ec61965b3d3
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2022-08-02 13:32:59 +0100

    await

--- src/Messaging.Host/Jobs/Listener.cs
+++ src/Messaging.Host/Jobs/Listener.cs
@@ -60,11 +60,11 @@
             this.channel = this.channelConfiguration.ConsumerChannel;
         }
 
-        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             this.logger.Info("Waiting for messages. To exit press CTRL+C");
             this.channel.BasicConsume(queue: $"{this.queueName}.q", autoAck: false, consumer: this.consumer);
-            return Task.CompletedTask;
+            await Task.CompletedTask;
         }
 
         public override Task StopAsync(CancellationToken stoppingToken)
