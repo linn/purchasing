26b0419d296a2d76fd3fe734a9743459b4993a7f
rosss <rosss@PC1329>
2022-08-04 14:18:03 +0100

    remove redundant Using

--- src/Facade/Services/ILeadTimesReportFacadeService.cs
+++ src/Facade/Services/ILeadTimesReportFacadeService.cs
@@ -1,7 +1,5 @@
 ﻿namespace Linn.Purchasing.Facade.Services
 {
-    using System.Collections.Generic;
-
     using Linn.Common.Facade;
     using Linn.Common.Reporting.Resources.ReportResultResources;
     using Linn.Purchasing.Resources.RequestResources;
