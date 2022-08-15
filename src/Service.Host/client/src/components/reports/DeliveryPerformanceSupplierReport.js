dd6a1bd1c6d8275c8d3d25e8dbff28754d4732df
Richard Phillips <richard.phillips@linn.co.uk>
2022-08-15 11:21:58 +0100

    remove unused imports

--- src/Service.Host/client/src/components/reports/DeliveryPerformanceSupplierReport.js
+++ src/Service.Host/client/src/components/reports/DeliveryPerformanceSupplierReport.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useLocation } from 'react-router';
 import {
-    Title,
     Page,
     ReportTable,
     BackButton,
