a97d37d870e9ca1cd7e68e5689bd8a9f09ca92c3
Richard Phillips <richard.phillips@linn.co.uk>
2022-08-22 16:09:43 +0100

    improve tests

--- tests/Unit/Domain.LinnApps.Tests/PurchaseOrderServiceTests/WhenEmailingPurchaseOrdersWithoutCopy.cs
+++ tests/Unit/Domain.LinnApps.Tests/PurchaseOrderServiceTests/WhenEmailingPurchaseOrdersWithoutCopy.cs
@@ -14,7 +14,7 @@
 
     using NUnit.Framework;
 
-    public class WhenEmailingPurchaseOrders : ContextBase
+    public class WhenEmailingPurchaseOrdersWithoutCopy : ContextBase
     {
         private ProcessResult result;
 
@@ -27,7 +27,7 @@
         [SetUp]
         public void SetUp()
         {
-            this.orders = new List<int> { 123, 456, 789, 101, 202 };
+            this.orders = new List<int> { 123 };
             this.userNumber = 808;
 
             this.order123 = new PurchaseOrder
@@ -53,50 +53,10 @@
                                                       new PurchaseOrderDetail { PartNumber = "P1" }
                                                   }
                                 };
+
             this.PurchaseOrderRepository.FindById(123).Returns(this.order123);
-            this.PurchaseOrderRepository.FindById(456).Returns(
-                new PurchaseOrder
-                    {
-                        OrderNumber = 456,
-                        AuthorisedById = 123,
-                        Cancelled = "Y",
-                        BaseOrderNetTotal = 12300,
-                        Details = new List<PurchaseOrderDetail> { new PurchaseOrderDetail { PartNumber = "P2" } }
-                    });
-            this.PurchaseOrderRepository.FindById(789).Returns(
-                new PurchaseOrder
-                    {
-                        OrderNumber = 789,
-                        AuthorisedById = null,
-                        BaseOrderNetTotal = 123,
-                        Details = new List<PurchaseOrderDetail> { new PurchaseOrderDetail { PartNumber = "P3" } }
-                    });
-            this.PurchaseOrderRepository.FindById(202).Returns(
-                new PurchaseOrder
-                    {
-                        OrderNumber = 202,
-                        AuthorisedById = 123,
-                        BaseOrderNetTotal = 123,
-                        Supplier = new Supplier
-                                       {
-                                           SupplierId = 777,
-                                           SupplierContacts =
-                                               new List<SupplierContact>
-                                                   {
-                                                       new SupplierContact
-                                                           {
-                                                               IsMainOrderContact = "Y",
-                                                           }
-                                                   }
-                                       },
-                    Details = new List<PurchaseOrderDetail> { new PurchaseOrderDetail { PartNumber = "P3" } }
-                    });
-            this.PurchaseOrderRepository.FindById(101)
-                .Returns((PurchaseOrder)null);
-            this.EmployeeRepository.FindById(this.userNumber)
-                .Returns(new Employee { FullName = "Fred", PhoneListEntry = new PhoneListEntry { EmailAddress = "fred@co" } });
             this.MiniOrderRepository.FindById(123).Returns(new MiniOrder { OrderNumber = 123 });
-            this.result = this.Sut.EmailMultiplePurchaseOrders(this.orders, this.userNumber, true);
+            this.result = this.Sut.EmailMultiplePurchaseOrders(this.orders, this.userNumber, false);
         }
 
         [Test]
@@ -110,11 +70,12 @@
         {
             var message = this.result.Message;
             message.Should().Contain("Order 123 emailed successfully to email777");
-            message.Should().Contain("Order 456 is cancelled");
-            message.Should().Contain("Order 789 is not authorised");
-            message.Should().Contain("Order 101 could not be found");
-            message.Should().Contain("Order 202 could not find order contact email");
-            message.Should().Contain("1 out of 5 emailed successfully");
+        }
+
+        [Test]
+        public void ShouldNotGetEmployee()
+        {
+            this.EmployeeRepository.DidNotReceive().FindById(Arg.Any<int>());
         }
 
         [Test]
@@ -124,7 +85,7 @@
                     "email777",
                     "email777",
                     Arg.Any<IEnumerable<Dictionary<string, string>>>(),
-                    Arg.Is<IEnumerable<Dictionary<string, string>>>(a => a.Any(b => b.ContainsValue("fred@co"))),
+                    Arg.Is<IEnumerable<Dictionary<string, string>>>(a => a.All(b => !b.ContainsValue("fred@co"))),
                     Arg.Any<string>(),
                     Arg.Any<string>(),
                     Arg.Any<string>(),
