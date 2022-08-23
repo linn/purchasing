1cd3a020b4593418c6e0d7bc9579373599d5c43c
Richard Phillips <richard.phillips@linn.co.uk>
2022-08-23 17:36:45 +0100

    improve test coverage of method

--- tests/Unit/Domain.LinnApps.Tests/PurchaseOrderServiceTests/WhenAuthorisingPurchaseOrders.cs
+++ tests/Unit/Domain.LinnApps.Tests/PurchaseOrderServiceTests/WhenAuthorisingPurchaseOrders.cs
@@ -23,7 +23,7 @@
         [SetUp]
         public void SetUp()
         {
-            this.orders = new List<int> { 123, 456, 789 };
+            this.orders = new List<int> { 123, 456, 789, 101 };
             this.userNumber = 808;
 
             this.order123 = new PurchaseOrder
@@ -50,6 +50,7 @@
                         BaseOrderNetTotal = 123,
                         Details = new List<PurchaseOrderDetail> { new PurchaseOrderDetail { PartNumber = "P3" } }
                     });
+            this.PurchaseOrderRepository.FindById(101).Returns((PurchaseOrder)null);
 
             this.PurchaseOrdersPack.OrderCanBeAuthorisedBy(123, null, this.userNumber, null, null, null)
                 .Returns(true);
@@ -72,7 +73,8 @@
             message.Should().Contain("Order 123 authorised successfully");
             message.Should().Contain("Order 456 YOU CANNOT AUTHORISE THIS ORDER");
             message.Should().Contain("Order 789 was already authorised");
-            message.Should().Contain("1 out of 3 authorised successfully");
+            message.Should().Contain("Order 101 could not be found");
+            message.Should().Contain("1 out of 4 authorised successfully");
         }
 
         [Test]
