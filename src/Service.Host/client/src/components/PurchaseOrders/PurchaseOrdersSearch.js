54502dc0a330720015776ed5b56bd33afd4a2d4f
rosss <rosss@PC1329>
2022-09-06 15:10:00 +0100

    Amend search on PurchOrder and PurchOrderReq

--- src/Service.Host/client/src/components/PurchaseOrders/PurchaseOrdersSearch.js
+++ src/Service.Host/client/src/components/PurchaseOrders/PurchaseOrdersSearch.js
@@ -1,21 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { collectionSelectorHelpers, Page, Typeahead } from '@linn-it/linn-form-components-library';
-import Typography from '@mui/material/Typography';
+import { Link as RouterLink } from 'react-router-dom';
+import {
+    collectionSelectorHelpers,
+    InputField,
+    Page,
+    utilities
+} from '@linn-it/linn-form-components-library';
+import { makeStyles } from '@mui/styles';
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
 import Grid from '@mui/material/Grid';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
 import moment from 'moment';
 import purchaseOrdersActions from '../../actions/purchaseOrdersActions';
 import history from '../../history';
 import config from '../../config';
 
 function PurchaseOrdersSearch() {
+    const useStyles = makeStyles(theme => ({
+        button: {
+            marginLeft: theme.spacing(1),
+            marginTop: theme.spacing(4)
+        },
+        a: {
+            textDecoration: 'none'
+        }
+    }));
+    const classes = useStyles();
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(purchaseOrdersActions.fetchState());
         dispatch(purchaseOrdersActions.search(''));
     }, [dispatch]);
-    const searchPurchaseOrders = searchTerm => dispatch(purchaseOrdersActions.search(searchTerm));
     const searchResults = useSelector(state =>
         collectionSelectorHelpers.getSearchItems(
             state.purchaseOrders,
@@ -25,9 +46,8 @@ function PurchaseOrdersSearch() {
             'orderNumber'
         )
     );
-    const searchLoading = useSelector(state =>
-        collectionSelectorHelpers.getSearchLoading(state.purchaseOrders)
-    );
+    const [orderNumber, setOrderNumber] = useState('');
+    const search = () => history.push(`/purchasing/purchase-orders/${orderNumber}/`);
 
     return (
         <Page history={history} homeUrl={config.appRoot}>
@@ -35,23 +55,61 @@ function PurchaseOrdersSearch() {
                 <Grid item xs={11}>
                     <Typography variant="h3">Search Purchase Orders</Typography>
                 </Grid>
-                <Grid item xs={12}>
-                    <Typeahead
-                        items={searchResults.map(x => ({
-                            ...x,
-                            description: `${moment(x.orderDate).format('DD MMM YYYY')} - ${
-                                x.supplier.name
-                            } (${x.supplier.id})`,
-                            href: x.links.find(l => l.rel === 'self')?.href
-                        }))}
-                        fetchItems={searchPurchaseOrders}
+                <Grid item xs={5}>
+                    <InputField
+                        fullWidth
                         placeholder="Search by Order Number"
-                        clearSearch={() => {}}
-                        resultLimit={100}
-                        loading={searchLoading}
-                        history={history}
-                        links
+                        value={orderNumber}
+                        label="Order Number"
+                        propertyName="orderNumber"
+                        onChange={(_, newValue) => setOrderNumber(newValue)}
+                        textFieldProps={{
+                            onKeyDown: data => {
+                                if (data.keyCode === 13 || data.keyCode === 9) {
+                                    search();
+                                }
+                            }
+                        }}
                     />
+                </Grid>
+                <Grid item xs={2}>
+                    <Button
+                        variant="outlined"
+                        color="primary"
+                        className={classes.button}
+                        onClick={search}
+                    >
+                        Go
+                    </Button>
+                </Grid>
+                <Grid item xs={12}>
+                    <List dense>
+                        {searchResults.map(x => (
+                            <Fragment key={x.orderNumber}>
+                                <Link
+                                    className={classes.a}
+                                    component={RouterLink}
+                                    to={utilities.getSelfHref(x)}
+                                >
+                                    <ListItem spacing={15}>
+                                        <Grid item xs={1}>
+                                            <Typography variant="subtitle1">
+                                                {x.orderNumber}
+                                            </Typography>
+                                        </Grid>
+                                        <Grid item xs={9}>
+                                            <Typography>
+                                                {`${moment(x.orderDate).format('DD MMM YYYY')} - ${
+                                                    x.supplier.name
+                                                } (${x.supplier.id})`}
+                                            </Typography>
+                                        </Grid>
+                                    </ListItem>
+                                </Link>
+                                <Divider component="li" />
+                            </Fragment>
+                        ))}
+                    </List>
                 </Grid>
             </Grid>
         </Page>
