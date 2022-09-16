bc7515de1f14f3670ccadff15e22e2c4f854d2a0
Andrew Fraser <andrewfraser73@hotmail.com>
2022-09-16 12:30:20 +0100

    tests

---
+++ tests/Integration/Integration.Tests/ChangeRequestModuleTests/ContextBase.cs
@@ -0,0 +1,54 @@
+﻿namespace Linn.Purchasing.Integration.Tests.ChangeRequestModuleTests
+{
+    using System.Net.Http;
+
+    using Linn.Common.Facade;
+    using Linn.Common.Persistence;
+    using Linn.Purchasing.Domain.LinnApps.Boms;
+    using Linn.Purchasing.Domain.LinnApps.Edi;
+    using Linn.Purchasing.Facade.ResourceBuilders;
+    using Linn.Purchasing.Facade.Services;
+    using Linn.Purchasing.IoC;
+    using Linn.Purchasing.Resources;
+    using Linn.Purchasing.Service.Modules;
+
+    using Microsoft.Extensions.DependencyInjection;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public class ContextBase
+    {
+        protected HttpClient Client { get; set; }
+
+        protected HttpResponseMessage Response { get; set; }
+
+        protected IFacadeResourceService<ChangeRequest, int, ChangeRequestResource, ChangeRequestResource> FacadeService { get; set; }
+
+        protected IRepository<ChangeRequest, int> Repository { get; set; }
+
+        protected IEdiOrderService MockDomainService { get; set; }
+
+        protected ITransactionManager TransactionManager { get; set; }
+
+        [SetUp]
+        public void SetUpContext()
+        {
+            this.Repository = Substitute.For<IRepository<ChangeRequest, int>>();
+            this.TransactionManager = Substitute.For<ITransactionManager>();
+            this.FacadeService = new ChangeRequestFacadeService(
+                this.Repository,
+                this.TransactionManager,
+                new ChangeRequestResourceBuilder());
+
+            this.Client = TestClient.With<ChangeRequestModule>(
+                services =>
+                    {
+                        services.AddSingleton(this.FacadeService);
+                        services.AddHandlers();
+                    },
+                FakeAuthMiddleware.EmployeeMiddleware);
+        }
+    }
+}
