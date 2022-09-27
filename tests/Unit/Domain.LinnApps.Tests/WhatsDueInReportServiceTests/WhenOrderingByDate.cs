2f14e37140dc3c477d2377598493cffe0fed17dd
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2022-09-27 17:10:35 +0100

    try fix test again

--- tests/Unit/Domain.LinnApps.Tests/WhatsDueInReportServiceTests/WhenOrderingByDate.cs
+++ tests/Unit/Domain.LinnApps.Tests/WhatsDueInReportServiceTests/WhenOrderingByDate.cs
@@ -26,7 +26,7 @@
             for (var i = 1; i < this.result.Rows.Count(); i++)
             {
                 var previousDateBooked = DateTime.ParseExact(
-                    this.result.GetGridTextValue(i - 1, 5), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                    this.result.GetGridTextValue(i - 1, 5), "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None);
                 DateTime.Parse(this.result.GetGridTextValue(i, 5)).Should().BeOnOrAfter(previousDateBooked);
             }
         }
