3d4e4c82d7b3e8b03de4aaa070e68bd5c1304395
Andrew Fraser <andrewfraser73@hotmail.com>
2022-10-17 14:41:28 +0100

    Chgbomtype server side (#404)
    * make work again
    
    * try taking out test completely
    
    * draft 1
    
    * pass through change id
    
    * integration test
    
    * tweaks

---
+++ src/Facade/ResourceBuilders/BomTypeChangeResourceBuilder.cs
@@ -0,0 +1,42 @@
+﻿namespace Linn.Purchasing.Facade.ResourceBuilders
+{
+    using System.Collections.Generic;
+
+    using Linn.Common.Facade;
+    using Linn.Purchasing.Domain.LinnApps.Parts;
+    using Linn.Purchasing.Resources;
+
+    public class BomTypeChangeResourceBuilder : IBuilder<BomTypeChange>
+    {
+        public BomTypeChangeResource Build(BomTypeChange model, IEnumerable<string> claims)
+        {
+            var resource = new BomTypeChangeResource
+                               {
+                                   PartNumber = model.PartNumber,
+                                   OldBomType = model.OldBomType,
+                                   OldSupplierId = model.OldSupplierId,
+                                   NewBomType = model.NewBomType,
+                                   NewSupplierId = model.NewSupplierId,
+                                   ChangedBy = model.ChangedBy
+                               };
+
+            if (model.Part != null)
+            {
+                resource.PartDescription = model.Part.Description;
+                resource.PartBomType = model.Part.BomType;
+                resource.PartCurrency = model.Part.Currency?.Name;
+                resource.PartCurrencyUnitPrice = model.Part.CurrencyUnitPrice;
+                resource.PartBaseUnitPrice = model.Part.BaseUnitPrice;
+            }
+
+            return resource;
+        }
+
+        public string GetLocation(BomTypeChange model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        object IBuilder<BomTypeChange>.Build(BomTypeChange entity, IEnumerable<string> claims) => this.Build(entity, claims);
+    }
+}
---
+++ tests/Unit/Domain.LinnApps.Tests/PartServiceTests/WhenChangingBomTypeWithInconsistentBomType.cs
@@ -0,0 +1,54 @@
+﻿namespace Linn.Purchasing.Domain.LinnApps.Tests.PartServiceTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+
+    using FluentAssertions;
+
+    using Linn.Purchasing.Domain.LinnApps.Exceptions;
+    using Linn.Purchasing.Domain.LinnApps.Parts;
+    using Linn.Purchasing.Domain.LinnApps.Parts.Exceptions;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public class WhenChangingBomTypeWithInconsistentBomType : ContextBase
+    {
+        private Action action;
+
+        private BomTypeChange bomTypeChange;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.authService
+                .HasPermissionFor(AuthorisedAction.ChangeBomType, Arg.Any<IEnumerable<string>>())
+                .Returns(true);
+
+            var part = new Part
+                           {
+                               PartNumber = "TEST 001",
+                               BomType = "A"
+                           };
+            this.partRepository.FindBy(Arg.Any<Expression<Func<Part, bool>>>()).Returns(part);
+
+            this.bomTypeChange = new BomTypeChange
+                                     {
+                                         PartNumber = "TEST 001",
+                                         OldBomType = "C",
+                                         NewBomType = "A"
+                                     };
+
+
+            this.action = () => this.Sut.ChangeBomType(this.bomTypeChange, new List<string>());
+        }
+
+        [Test]
+        public void ShouldThrowInvalidBomTypeChangeException()
+        {
+            this.action.Should().Throw<InvalidBomTypeChangeException>().WithMessage("Inconsistent old bom type should be A was C");
+        }
+    }
+}
