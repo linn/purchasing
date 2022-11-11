f0544739e3f2589c07c4d21ee02500c89dac3311
Richard Phillips <richard.phillips@linn.co.uk>
2022-11-11 10:06:45 +0000

    board search

--- src/Service.Host/client/src/components/boards/BoardSearch.js
+++ src/Service.Host/client/src/components/boards/BoardSearch.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Grid from '@mui/material/Grid';
-
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
 import { useSelector, useDispatch } from 'react-redux';
 import { Page, collectionSelectorHelpers, Search } from '@linn-it/linn-form-components-library';
 import boardsActions from '../../actions/boardsActions';
@@ -25,34 +27,52 @@ function BoardSearch() {
         collectionSelectorHelpers.getSearchLoading(state.boards)
     );
 
-    const goToBoard = selectedBoard => {
-        var a = selectedBoard;
-        history.push(`${boardItemType.uri}/424`);
+    const goToSelectedBoard = selectedBoard => {
+        history.push(`${boardItemType.uri}/${selectedBoard.boardCode}`);
+    };
+
+    const goToBoard = () => {
+        history.push(`${boardItemType.uri}/${board}`);
     };
 
     return (
-        <Page history={history} homeUrl={config.appRoot}>
+        <Page history={history} style={{ paddingBottom: '20px' }} homeUrl={config.appRoot}>
+            <Typography variant="h5" gutterBottom>
+                Search or select PCAS board
+            </Typography>
             <Grid container spacing={2}>
                 <Grid item xs={12}>
-                    <Search
-                        propertyName="boardCode"
-                        label="Select Board"
-                        resultsInModal
-                        resultLimit={100}
-                        value={board}
-                        handleValueChange={(_, b) => setBoard(b)}
-                        search={searchBoards}
-                        searchResults={searchBoardsResults.map(s => ({
-                            ...s,
-                            id: `${s.boardCode}`
-                        }))}
-                        loading={searchBoardsLoading}
-                        priorityFunction="closestMatchesFirst"
-                        onResultSelect={newValue => {
-                            goToBoard(newValue);
-                        }}
-                        clearSearch={clearSearchBoards}
-                    />
+                    <Stack direction="row" spacing={2}>
+                        <Search
+                            propertyName="boardCode"
+                            label="Select Board"
+                            resultsInModal
+                            resultLimit={100}
+                            value={board}
+                            handleValueChange={(_, b) => setBoard(b)}
+                            search={searchBoards}
+                            helperText="Press <ENTER> to search or GO button to go directly"
+                            searchResults={searchBoardsResults.map(s => ({
+                                ...s,
+                                id: `${s.boardCode}`,
+                                name: `${s.boardCode}`
+                            }))}
+                            loading={searchBoardsLoading}
+                            priorityFunction="closestMatchesFirst"
+                            onResultSelect={newValue => {
+                                goToSelectedBoard(newValue);
+                            }}
+                            clearSearch={clearSearchBoards}
+                        />
+                        <Button
+                            variant="outlined"
+                            onClick={goToBoard}
+                            size="small"
+                            style={{ marginBottom: '25px' }}
+                        >
+                            Go
+                        </Button>
+                    </Stack>
                 </Grid>
             </Grid>
         </Page>
