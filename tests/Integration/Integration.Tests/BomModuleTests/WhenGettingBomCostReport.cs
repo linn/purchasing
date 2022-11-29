33d335a45bf8058dd14892a880b6c651d749a4c2
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2022-11-29 10:00:59 +0000

    add some tests

---
+++ tests/Integration/Integration.Tests/BomModuleTests/WhenGettingBomCostReport.cs
@@ -0,0 +1,48 @@
+﻿namespace Linn.Purchasing.Integration.Tests.BomModuleTests
+{
+    using System.Collections.Generic;
+    using System.Net;
+
+    using FluentAssertions;
+
+    using Linn.Purchasing.Domain.LinnApps.Boms.Models;
+    using Linn.Purchasing.Integration.Tests.Extensions;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public class WhenGettingBomCostReport : ContextBase
+    {
+        private IEnumerable<BomCostReport> results;
+
+        private string bomName;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.results = new List<BomCostReport> { new BomCostReport { SubAssembly = "SK HUB" } };
+
+            this.MockBomReportsDomainService.GetBomCostReport(this.bomName, true, 999, 15).Returns(this.results);
+
+            this.Response = this.Client.Get(
+                $"/purchasing/boms/reports/cost?bomName={this.bomName}" 
+                + $"&splitBySubAssembly=True&levels=999&labourHourlyRate=15",
+                with => { with.Accept("application/json"); }).Result;
+        }
+
+        [Test]
+        public void ShouldReturnOk()
+        {
+            this.Response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+
+        [Test]
+        public void ShouldPassCorrectOptionsToDomainService()
+        {
+            this.MockBomReportsDomainService.Received().GetBomCostReport(
+                this.bomName.ToUpper().Trim(), true, 999, 15);
+        }
+    }
+}
--- tests/Unit/Domain.LinnApps.Tests/BomReportsServiceTests/WhenGettingBomCostsReportSplitBySubAssembly.cs
+++ tests/Unit/Domain.LinnApps.Tests/BomReportsServiceTests/WhenGettingBomCostsReportSplitBySubAssembly.cs
@@ -5,6 +5,8 @@
     using System.Linq;
     using System.Linq.Expressions;
 
+    using FluentAssertions;
+
     using Linn.Purchasing.Domain.LinnApps.Boms.Models;
 
     using NSubstitute;
@@ -13,6 +15,8 @@
 
     public class WhenGettingBomCostsReportSplitBySubAssembly : ContextBase
     {
+        private IEnumerable<BomCostReport> results;
+
         [SetUp]
         public void SetUp()
         {
@@ -24,16 +28,52 @@
                                   new BomTreeNode { Id = 4 },
                                   new BomTreeNode { Id = 5 },
                                   new BomTreeNode { Id = 6 },
-                                  new BomTreeNode { Id = 7 }
+                                  new BomTreeNode { Id = 7 },
+                                  new BomTreeNode { Id = 8 },
+                                  new BomTreeNode { Id = 9 },
+                                  new BomTreeNode { Id = 10 }
                               };
-            this.BomTreeService.FlattenBomTree("SK HUB", null, true, false).Returns(flatBom);
+            this.BomTreeService.FlattenBomTree("SK HUB", 999).Returns(flatBom);
             var details = new List<BomCostReportDetail>
                               {
-                                  new BomCostReportDetail { DetailId = 3 }
+                                  new BomCostReportDetail { DetailId = 1, BomName = "SK HUB", PartNumber = "MCAS 073" },
+                                  new BomCostReportDetail { DetailId = 2, BomName = "SK HUB", PartNumber = "CONN 493" },
+                                  new BomCostReportDetail { DetailId = 3, BomName = "SK HUB", PartNumber = "SELEKT BITS" },
+                                  new BomCostReportDetail { DetailId = 4, BomName = "SK HUB", PartNumber = "MCP 1000" },
+                                  new BomCostReportDetail { DetailId = 5, BomName = "SK HUB", PartNumber = "PCAS 1234" },
+                                  new BomCostReportDetail { DetailId = 6, BomName = "MCAS 073", PartNumber = "RES 516" },
+                                  new BomCostReportDetail { DetailId = 7, BomName = "SELEKT BITS", PartNumber = "SELEKT DIAL" },
+                                  new BomCostReportDetail { DetailId = 8, BomName = "SELEKT DIAL", PartNumber = "SCREW" },
+                                  new BomCostReportDetail { DetailId = 9, BomName = "PRINTING BITS", PartNumber = "SELEKT BITS" },
+                                  new BomCostReportDetail { DetailId = 10, BomName = "PRINTING BITS", PartNumber = "INK" },
+                                
                               }.AsQueryable();
-
             this.BomCostReportDetailsRepository.FilterBy(Arg.Any<Expression<Func<BomCostReportDetail, bool>>>())
                 .Returns(details);
+
+            this.results = this.Sut.GetBomCostReport("SK HUB", true, 999, 15);
+        }
+
+        [Test]
+        public void ShouldReturnReportGroupedBySubAssembly()
+        {
+            // five different assemblies: SK HUB, MCAS 073, SELEKT BITS, PRINTING BITS, SELEKT DIAL
+            this.results.Count().Should().Be(5);
+            this.results.ElementAt(0).Breakdown.Rows.Count().Should().Be(5); // five things on the SK HUB bom
+            this.results.ElementAt(1).Breakdown.Rows.Count().Should().Be(1); // one thing on the MCAS 073 bom
+            this.results.ElementAt(2).Breakdown.Rows.Count().Should().Be(1); // one thing on the SELEKT BITS bom
+            this.results.ElementAt(3).Breakdown.Rows.Count().Should().Be(1); // one thing on the SELEKT DIAL bom
+            this.results.ElementAt(4).Breakdown.Rows.Count().Should().Be(2); // two things on the PRINTING BITS bom
+        }
+
+        [Test]
+        public void ShouldPreserveTreeOrder()
+        {
+            this.results.First().SubAssembly.Should().Be("SK HUB");
+            this.results.ElementAt(1).SubAssembly.Should().Be("MCAS 073");
+            this.results.ElementAt(2).SubAssembly.Should().Be("SELEKT BITS");
+            this.results.ElementAt(3).SubAssembly.Should().Be("SELEKT DIAL");
+            this.results.ElementAt(4).SubAssembly.Should().Be("PRINTING BITS");
         }
     }
 }
