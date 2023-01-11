d2964283c1c9c06075c8da4f8948992ed2f9fedc
Lewis Renfrew <LewisRe@linn.co.uk>
2023-01-11 12:04:25 +0000

    int test

---
+++ tests/Integration/Integration.Tests/BomModuleTests/WhenCopyingBom.cs
@@ -0,0 +1,55 @@
+﻿namespace Linn.Purchasing.Integration.Tests.BomModuleTests
+{
+    using System.Net;
+    using System.Net.Http.Json;
+    using FluentAssertions;
+
+    using Linn.Purchasing.Integration.Tests.Extensions;
+    using Linn.Purchasing.Resources;
+    using Linn.Purchasing.Resources.Boms;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public class WhenCopyingBom : ContextBase
+    {
+        private CopyBomResource resource;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.resource = new CopyBomResource { SrcPartNumber = "SRC", DestPartNumber = "DEST", CrfNumber = 123 };
+
+            this.Response = this.Client.PostAsJsonAsync(
+                $"/purchasing/boms/copy",
+                this.resource).Result;
+        }
+
+        [Test]
+        public void ShouldReturnJsonContentType()
+        {
+            this.Response.Content.Headers.ContentType.Should().NotBeNull();
+            this.Response.Content.Headers.ContentType?.ToString().Should().Be("application/json");
+        }
+
+        [Test]
+        public void ShouldReturnOk()
+        {
+            this.Response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Test]
+        public void ShouldCommit()
+        {
+            this.TransactionManager.Received().Commit();
+        }
+
+        [Test]
+        public void ShouldBuildResource()
+        {
+            var result = this.Response.DeserializeBody<ProcessResultResource>();
+            result.Success.Should().BeTrue();
+        }
+    }
+}
