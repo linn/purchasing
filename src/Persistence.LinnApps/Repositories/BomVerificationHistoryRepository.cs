5f5893f23c08d2d555d40cd20b81a0eea42d0bc4
rosss <rosss@PC1329>
2023-02-14 18:48:19 +0000

    Search

---
+++ src/Persistence.LinnApps/Repositories/BomVerificationHistoryRepository.cs
@@ -0,0 +1,29 @@
+﻿namespace Linn.Purchasing.Persistence.LinnApps.Repositories
+{
+    using System;
+    using System.Linq;
+    using System.Linq.Expressions;
+
+    using Linn.Common.Persistence.EntityFramework;
+    using Linn.Purchasing.Domain.LinnApps.Boms;
+
+    using Microsoft.EntityFrameworkCore;
+
+    public class BomVerificationHistoryRepository : EntityFrameworkRepository<BomVerificationHistory, int>
+    {
+        private readonly ServiceDbContext serviceDbContext;
+
+        public BomVerificationHistoryRepository(ServiceDbContext serviceDbContext)
+            : base(serviceDbContext.BomVerificationHistory)
+        {
+            this.serviceDbContext = serviceDbContext;
+        }
+
+        public override IQueryable<BomVerificationHistory> FilterBy(Expression<Func<BomVerificationHistory, bool>> expression)
+        {
+            return this.serviceDbContext.BomVerificationHistory
+                .Include(x => x.PartNumber)
+                .Where(expression);
+        }
+    }
+}
