36474b2af90bad5639e2581e1523ffe3cdc62d9d
lewisrenfrew <Lewis.Renfrew@linn.co.uk>
2023-04-21 12:00:46 +0100

    full steam ahead (#612)
    * full steam ahead
    
    * get order number right in test

--- tests/Integration/Integration.Scheduling.Tests/PurchaseOrderRemindersSchedulerTests/When8Am.cs
+++ tests/Integration/Integration.Scheduling.Tests/PurchaseOrderRemindersSchedulerTests/When8Am.cs
@@ -34,7 +34,6 @@
             this.Dispatcher.Received().Dispatch(Arg.Is<EmailPurchaseOrderReminderMessageResource>(x =>
                 x.Deliveries.Count() == 2 && x.Deliveries.All(d => d.OrderNumber == 1 || d.OrderNumber == 12345)));
 
-
             // since this delivery is not advised to arrive in two days
             this.Dispatcher.DidNotReceive().Dispatch(
                 Arg.Is<EmailPurchaseOrderReminderMessageResource>(
@@ -54,6 +53,11 @@
             this.Dispatcher.DidNotReceive().Dispatch(
                 Arg.Is<EmailPurchaseOrderReminderMessageResource>(
                     x => x.Deliveries.Any(d => d.OrderNumber == 5)));
+
+            // since this order is cancelled 
+            this.Dispatcher.DidNotReceive().Dispatch(
+                Arg.Is<EmailPurchaseOrderReminderMessageResource>(
+                    x => x.Deliveries.Any(d => d.OrderNumber == 789)));
         }
     }
 }
