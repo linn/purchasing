54b75a2fcd14e79747c822d4042e4146e883b1e6
Richard Phillips <richardiphillips@users.noreply.github.com>
2023-09-14 09:33:46 +0100

    fix debit note to take original purchase price as per ticket 17849 (#646)
    * New csv content type examples (#589)
    
    * update all the things
    
    * fix tests and stuff
    
    * comment
    
    * var names
    
    * fix bug that new tighter EFCore caught :)
    
    * fix dockerfile up
    
    * fix the other two dockerfiles
    
    * delete console log
    
    * delete my .MapPatch() extension now that Microsoft have implemented it
    
    * try x64
    
    * tidy
    
    * proxy updates
    
    * all the other updates
    
    * tidy
    
    * tidy
    
    * fix test
    
    * remove run configs
    
    ---------
    
    Co-authored-by: Lewis Renfrew <LewisRe@linn.co.uk>
    
    * fix debit note to take original purchase price as per ticket 17849
    
    ---------
    
    Co-authored-by: lewisrenfrew <Lewis.Renfrew@linn.co.uk>
    Co-authored-by: Lewis Renfrew <LewisRe@linn.co.uk>

--- tests/Unit/Domain.LinnApps.Tests/PlCreditDebitNoteServiceTests/WhenCreatingANoteFromOrder.cs
+++ tests/Unit/Domain.LinnApps.Tests/PlCreditDebitNoteServiceTests/WhenCreatingANoteFromOrder.cs
@@ -13,11 +13,47 @@
     {
         private PurchaseOrder order;
 
+        private int originalOrderNumber;
+
+        private PurchaseOrder originalOrder;
+
         [SetUp]
         public void SetUp()
         {
+            this.originalOrderNumber = 456;
+            this.originalOrder = new PurchaseOrder
+                             {
+                                 OrderNumber = this.originalOrderNumber,
+                                 DocumentType = new DocumentType { Name = "CO" },
+                                 DocumentTypeName = "CO",
+                                 SupplierId = 1,
+                                 EnteredById = 8,
+                                 Details = new List<PurchaseOrderDetail>
+                                               {
+                                                   new PurchaseOrderDetail
+                                                       {
+                                                           OrderNumber = this.originalOrderNumber,
+                                                           Line = 11,
+                                                           BaseOrderUnitPrice = 3,
+                                                           BaseOurUnitPrice = 3,
+                                                           OrderQty = 1,
+                                                           OurQty = 1,
+                                                           BaseDetailTotal = 3,
+                                                           DetailTotalCurrency = 3,
+                                                           OrderUnitPriceCurrency = 3,
+                                                           OurUnitPriceCurrency = 3,
+                                                           NetTotalCurrency = 3,
+                                                           VatTotalCurrency = 0,
+                                                           BaseVatTotal = 0,
+                                                           BaseNetTotal = 3
+                                                       }
+                                               }
+                             };
+
             this.MockNoteTypeRepository.FindById("D").Returns(new CreditDebitNoteType { Type = "D" });
             this.MockSupplierRepository.FindById(1).Returns(new Supplier { SupplierId = 1 });
+            this.MockOrderRepository.FindById(this.originalOrderNumber)
+                .Returns(this.originalOrder);
             this.order = new PurchaseOrder
                              {
                                  OrderNumber = 123,
@@ -31,7 +67,7 @@
                                                        {
                                                            OrderNumber = 123,
                                                            Line = 1,
-                                                           OriginalOrderNumber = 456,
+                                                           OriginalOrderNumber = this.originalOrderNumber,
                                                            OriginalOrderLine = 11,
                                                            BaseOrderUnitPrice = 2,
                                                            BaseOurUnitPrice = 2,
@@ -66,6 +102,9 @@
                     a => a.NoteType.Type == "D" 
                          && a.OriginalOrderNumber == 456 
                          && a.OriginalOrderLine == 11
+                         && a.NetTotal == 3m
+                         && a.OrderUnitPrice == 3m
+                         && a.Total == 3m
                          && a.CreditOrReplace == "CREDIT"));
         }
     }
