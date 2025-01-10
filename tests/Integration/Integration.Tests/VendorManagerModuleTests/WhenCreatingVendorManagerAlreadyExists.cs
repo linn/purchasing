51fe0514c31f040015d8875ce4a09def4d72fb44
OliverMclachlan <146720368+OliverMclachlan@users.noreply.github.com>
2025-01-10 16:57:46 +0000

    Adding more tests

--- tests/Integration/Integration.Tests/VendorManagerModuleTests/WhenCreatingVendorManagerAlreadyExists.cs
+++ tests/Integration/Integration.Tests/VendorManagerModuleTests/WhenCreatingVendorManagerAlreadyExists.cs
@@ -1,7 +1,9 @@
 ﻿namespace Linn.Purchasing.Integration.Tests.VendorManagerModuleTests
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Linq.Expressions;
     using System.Net;
     using System.Net.Http.Json;
 
@@ -16,33 +18,31 @@
 
     using NUnit.Framework;
 
-    public class WhenCreatingVendorManager : ContextBase
+    public class WhenCreatingVendorManagerAlreadyExists : ContextBase
     {
         private string id;
 
         private VendorManagerResource vendorManager;
+
+        private VendorManager currentVendorManagers;
+        
 
         [SetUp]
         public void SetUp()
         {
             this.id = "M";
             this.vendorManager = new VendorManagerResource { VmId = this.id, Name = "vm1", PmMeasured = "Y", UserNumber = 10 };
+            this.currentVendorManagers = new VendorManager { Id = this.id, PmMeasured = "Y", UserNumber = 10 };
+
+            this.VendorManagerRepository.FindBy(Arg.Any<Expression<Func<VendorManager, bool>>>()).Returns(this.currentVendorManagers);
 
             this.Response = this.Client.PostAsJsonAsync("/purchasing/vendor-managers", this.vendorManager).Result;
         }
 
         [Test]
-        public void ShouldReturnOk()
+        public void ShouldReturnBadRequest()
         {
-            this.Response.StatusCode.Should().Be(HttpStatusCode.Created);
-        }
-
-        [Test]
-        public void ShouldReturnProcessResultWithSuccess()
-        {
-            var result = this.Response.DeserializeBody<VendorManagerResource>();
-            result.PmMeasured.Should().Contain("Y");
-            result.UserNumber.Should().Be(10);
+            this.Response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
         }
     }
 }
---
+++ tests/Integration/Integration.Tests/VendorManagerModuleTests/WhenUpdatingVendorManagerAlreadyExists.cs
@@ -0,0 +1,69 @@
+﻿namespace Linn.Purchasing.Integration.Tests.VendorManagerModuleTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Net;
+    using System.Net.Http.Json;
+
+    using FluentAssertions;
+
+    using Linn.Purchasing.Domain.LinnApps;
+    using Linn.Purchasing.Domain.LinnApps.Suppliers;
+    using Linn.Purchasing.Integration.Tests.Extensions;
+    using Linn.Purchasing.Resources;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public class WhenUpdatingVendorManagerAlreadyExists : ContextBase
+    {
+        private string id;
+
+        private VendorManagerResource vendorManager;
+
+        private VendorManager currentVendorManagers;
+
+        private List<VendorManager> vendorManagers;
+
+        private VendorManagerResource updatedVendorManager;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.id = "M";
+            this.vendorManager = new VendorManagerResource { VmId = this.id, Name = "vm1", PmMeasured = "Y", UserNumber = 10 };
+            this.updatedVendorManager = new VendorManagerResource { VmId = this.id, Name = "vm2", PmMeasured = "Y", UserNumber = 20 };
+
+            this.vendorManagers = new List<VendorManager>
+                                      {
+                                          new VendorManager
+                                              {
+                                                  Id = "A",
+                                                  PmMeasured = "Y",
+                                                  UserNumber = 20,
+                                                  Employee = new Employee { FullName = "vm2", Id = 20 }
+                                              },
+                                          new VendorManager
+                                              {
+                                                  Id = "B",
+                                                  PmMeasured = "Y",
+                                                  UserNumber = 30,
+                                                  Employee = new Employee { FullName = "vm3", Id = 30 }
+                                              }
+                                      };
+
+            this.VendorManagerRepository.FindBy(Arg.Any<Expression<Func<VendorManager, bool>>>()).Returns(this.currentVendorManagers);
+
+            this.Response = this.Client.PutAsJsonAsync($"/purchasing/vendor-managers/{this.id}", this.updatedVendorManager).Result;
+        }
+
+        [Test]
+        public void ShouldReturnBadRequest()
+        {
+            this.Response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+    }
+}
