0d2398fff941f6a6910ededbf8f4c72c999dbbaa
Lewis <lewis.renfrew@linn.co.uk>
2025-10-16 10:01:44 +0100

    see if this works

---
+++ src/Service/Models/ViewModel.js.cs
@@ -0,0 +1,9 @@
+namespace Linn.Purchasing.Service.Models
+{
+    public class ViewModel
+    {
+        public string AppSettings { get; set; }
+
+        public string BuildNumber { get; set; }
+    }
+}
